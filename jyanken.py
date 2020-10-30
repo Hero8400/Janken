ba7c7d51d8f10f4872434bd1603299d8d1ade693
HeroK8400 <kamikazeboy8410@gmail.com>
2020-10-30 23:30:53 +0900

    ケイスケモード追加

--- jyanken.py
+++ jyanken.py
@@ -2,8 +2,12 @@
 
 import random
 
+player_dic = {"n": "一般人", "h": "ケイスケ・ホンダ"}
 dic = {"a": "グー", "b": "チョキ", "c": "パー"}
-
+print("誰と戦う？")
+print("n=一般人 h=ケイスケ・ホンダ")
+player = input('>>> ')
+player = player.lower()
 print("じゃんけん！！")
 print("a=グー b=チョキ c=パー a,b,cから選んでね")
 
@@ -11,6 +15,7 @@ user = input('>>> ')
 user = user.lower()
 
 try:
+    player_choice = player_dic[player]
     user_choice = dic[user]
 
     choice_list = ["a", "b", "c"]
@@ -19,22 +24,26 @@ try:
     draw = 'DRAW'
     win = 'You Win!!'
     lose = 'You Lose!!'
+    if player == 'h' :
+        pc ="ペプシコーラ"
+        judge = lose
+    else :
+        if user_choice == pc:
+            judge = draw
+        else:
+            if user_choice == "グー":
+                if pc == "チョキ":
+                    judge = win
+                else:
+                    judge = lose
+            elif user_choice == "チョキ":
+                if pc == "パー":
+                    judge = win
+                else:
+                    judge = lose
 
-    if user_choice == pc:
-        judge = draw
-    else:
-        if user_choice == "グー":
-            if pc == "チョキ":
-                judge = win
-            else:
-                judge = lose
-        elif user_choice == "チョキ":
-            if pc == "パー":
-                judge = win
-            else:
-                judge = lose
-
-    print("君 VS 相手")
+    print(f"対戦相手はこの人！　{player_choice}")
+    print("いざ勝負!!")
     print(f"{user_choice} VS {pc}")
     print(judge)
     if judge == lose:

78aa32927c7214a00f31625fa05d001640faf295
HeroK8400 <kamikazeboy8410@gmail.com>
2020-10-31 21:49:57 +0900

    関数を分けました。

--- jyanken.py
+++ jyanken.py
@@ -3,6 +3,30 @@
 import random
 
 
+judge_dic = {"draw": "DRAW", "win": "You WIN!!", "lose": "You LOSE!!"}
+
+
+def gameResult(user_choice, pc, player):
+    if player == 'h':
+        pc = "ペプシコーラ"
+        judge = judge_dic["lose"]
+    else:
+        if user_choice == pc:
+            judge = judge_dic["draw"]
+        else:
+            if user_choice == "グー":
+                if pc == "チョキ":
+                    judge = judge_dic["win"]
+                else:
+                    judge = judge_dic["lose"]
+            elif user_choice == "チョキ":
+                if pc == "パー":
+                    judge = judge_dic["win"]
+                else:
+                    judge = judge_dic["lose"]
+    return judge
+
+
 def main():
     """メイン処理
     """
@@ -30,36 +54,16 @@ def main():
 
     player_choice = player_dic[player]
     user_choice = dic[user]
-
     choice_list = list(dic.keys())
     pc = dic[random.choice(choice_list)]
 
-    draw = 'DRAW'
-    win = 'You Win!!'
-    lose = 'You Lose!!'
-    if player == 'h':
-        pc = "ペプシコーラ"
-        judge = lose
-    else:
-        if user_choice == pc:
-            judge = draw
-        else:
-            if user_choice == "グー":
-                if pc == "チョキ":
-                    judge = win
-                else:
-                    judge = lose
-            elif user_choice == "チョキ":
-                if pc == "パー":
-                    judge = win
-                else:
-                    judge = lose
+    judge = gameResult(user_choice, pc, player)
 
     print(f"対戦相手はこの人！　{player_choice}")
     print("いざ勝負!!")
     print(f"{user_choice} VS {pc}")
     print(judge)
-    if judge == lose:
+    if judge == judge_dic["lose"]:
         print("何で負けたか明日までに考えといてください。")
         print("ほないただきます。ﾌﾟｼｬｰｰｰｰ！！！！！")
 

435251ed8b508af4b146d8fef73ab2c7c4c4e3c3
HeroK8400 <kamikazeboy8410@gmail.com>
2020-11-21 23:47:00 +0900

    適当にピックするだけの処理まで作成

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 """ブラックジャックのルール
-1.　初期カードは52枚
-2.  引く際にカードの重複は無いようにする
+1.　初期カードは52枚 済
+2.  引く際にカードの重複は無いようにする 済
 3.  プレイヤーとディーラーの2人対戦。
 4.  プレイヤーは実行者、ディーラーは自動的に実行
 5.  実行開始時、プレイヤーとディーラーはそれぞれ、カードを2枚引く。
@@ -15,10 +15,12 @@
 12. ダブルダウンなし、スピリットなし、サレンダーなし、その他特殊そうなルールなし
 """
 from enum import Enum
+from typing import List
 import random
 import pprint
 
 #2020/11/18 目標：カードを一枚引くとこまで。
+
 class Suit(Enum):
     HEART = ("♥")
     SPADE = ("♠")
@@ -38,31 +40,66 @@ class Card:
     def __init__(self, suit: Suit, num: int):
         self.suit = suit
         self.num = num
-    
-    def deck():
-        #self.suit = {'heart':"♥",'spade':"♠", 'diamond':"♦",'club':"♣"}
-        cardNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+#山札
+Deck: List[Card]
+#手札
+PlayerHands: List[Card]
 
 def main():
+    global Deck
+    setUp()
     suit = Suit.HEART
     num = 3
     card = Card(suit,num)
     print(f"選んだカード：{card.suit.icon} の {card.num}")
-    deck = initDeck()
-    random.shuffle(deck)
-    pprint.pprint(deck)
-    pick = deck[0]
-    print(vars(pick))
-    print(pick.suit)
+    #pprint.pprint(deck)
 
-def initDeck():
-    deck = []    
+    #↓Python流do_while文
+    while True:
+        pick()
+        if not choiceAction():
+            break
+    #print(pick.suit)
+
+def setUp():
+    global Deck
+    global PlayerHands
+    Deck = []
+    PlayerHands = []
     for suit in Suit:
         for number in range(13):
             card = Card(suit, number + 1)
-            deck.append(card)
+            Deck.append(card)
 
-    return deck
+    random.shuffle(Deck)
+
+def choiceAction() -> bool:
+    point: int = sum(map(lambda x: x.num, PlayerHands))
+    if point > 21:
+        print("bust!!!!!!!!")
+        return False
+    elif point ==  21:
+        print("BlackJack!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        return False
+    
+    print("更にカードを引きますか？[y/n]")
+    action = input('>>> ')
+
+    return action.lower() == 'y'
+    # ↓上記の処理の意味
+    # if action.lower() == 'y':
+    #     return True
+    # else:
+    #     return False
+
+def pick():
+    global Deck
+    global PlayerHands
+    PlayerHands.append(Deck.pop(0))
+    # pick = Deck.pop(random.randint(0, len(Deck - 1)))
+    print(f"あなたが引いたカード:{vars(PlayerHands[-1])}")
+    print(f"現在の合計値：{sum(map(lambda x: x.num, PlayerHands))}")
 
 
 if __name__ == "__main__":
